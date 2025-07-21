531f949733d219edc483714c70c1a3f29fae71cd
Lucas Valério <126265243+LcValerioo@users.noreply.github.com>
2025-07-21 00:30:39 +0000

    Implementacao da Normal

--- variaveis_aleatorias_continuas.py
+++ variaveis_aleatorias_continuas.py
@@ -36,3 +36,43 @@ prob = expon.cdf(x, scale=1/lambda_)
 print(f"O resultado é {prob:.3f}")
 print(f"Em porcentagem: {100* prob:.1f}%")
 '''
+
+#Distribuição Normal
+'''
+from scipy.stats import norm
+import numpy as np
+import matplotlib.pyplot as plt
+'''
+'''
+#Definir média e desvio padrão
+mu = float(input("Digite a Média: "))
+sigma = float(input("Digite o Desvio Padrão: "))
+
+#Definir o valor minimo de retorno desejado
+x = float(input("Digite o valor X: "))
+
+#Calcular a probabilidade com scipy.stats
+prob = 1 - norm.cdf(x, loc=mu, scale=sigma)
+
+#Exibir a probabilidade 
+print(f"A Probabilidade desse evento {x:.0f} ocorrer é de {prob:.2f}")
+'''
+'''
+#Parâmetros da distribuição normal
+mu = float(input("Digite a Média: "))
+sigma = float(input("Digite o Desvio Padrão: "))
+
+#Intervalo de alturas para o cálculo da densidade de probabilidade
+alturas = np.linspace(mu - 4 * sigma, mu + 4 * sigma, 1000)
+
+#Calculo da densidade de probabilidade usando a função norm.pdf
+densidade_probabilidade = norm.pdf(alturas, loc=mu, scale=sigma)
+
+#Criação do gráfico
+plt.plot(alturas, densidade_probabilidade)
+plt.xlabel('Alturas (cm)')
+plt.ylabel('Densidade de probabilidade')
+plt.title('Distribuição normal para a altura de pessoas adultas')
+plt.grid(True)
+plt.show()
+'''
